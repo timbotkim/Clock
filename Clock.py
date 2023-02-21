23409dcd55a16fd5ff4fdd84bb1378b011d01075
timbotkim <107649760+timbotkim@users.noreply.github.com>
2023-02-20 23:41:33 -0500

    Update Clock.py

--- Clock.py
+++ Clock.py
@@ -5,9 +5,9 @@ app_window.title("Digital Clock")
 app_window.geometry("420x150") 
 app_window.resizable(1,1)
 
-text_font= ("Boulder", 68, 'bold')
-background = "#ccccff"
-foreground= "#363529"
+text_font= ("Times New Roman", 68, 'bold')
+background = "#004225"
+foreground= "#C4A484"
 border_width = 25
 
 label = Label(app_window, font=text_font, bg=background, fg=foreground, bd=border_width) 
